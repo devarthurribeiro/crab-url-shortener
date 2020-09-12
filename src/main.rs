d12168f7c05512c00142b4569bdda3d31466461d
Arthur Ribeiro <devarthurribeiro@gmail.com>
2020-09-12 18:31:22 -0300

    riderect urls and create not found page

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,17 @@ use warp::Filter;
 async fn main() {
     let db = models::blank_db();
 
-    let api = filters::urls(db);
+    let site = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::dir("./src/html/index.html"));
 
-    warp::serve(api)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    let not_found = warp::path("not_found")
+        .and(warp::path::end())
+        .and(warp::fs::file("./src/html/not_found.html"));
+
+    let api = filters::urls(db).or(not_found).or(site);
+
+    warp::serve(api).run(([127, 0, 0, 1], 3030)).await;
 }
 
 mod filters {
@@ -20,16 +26,28 @@ mod filters {
     pub fn urls(
         db: Db,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        url_create(db.clone())
+        url_create(db.clone()).or(url_get(db.clone()))
     }
 
-    pub fn url_create(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        warp::path!("urls")
-        .and(warp::post())
-        .and(json_body())
-        .and(with_db(db))
-        .and_then(handlers::create_url)
+    pub fn url_get(
+        db: Db,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::path!("urls" / String)
+            .and(warp::get())
+            .and(with_db(db))
+            .and_then(handlers::get_url)
     }
+
+    pub fn url_create(
+        db: Db,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::path!("urls")
+            .and(warp::post())
+            .and(json_body())
+            .and(with_db(db))
+            .and_then(handlers::create_url)
+    }
+
     fn with_db(db: Db) -> impl Filter<Extract = (Db,), Error = std::convert::Infallible> + Clone {
         warp::any().map(move || db.clone())
     }
@@ -41,12 +59,23 @@ mod filters {
 mod handlers {
     use super::models::{Db, Url};
     use std::convert::Infallible;
-    use warp::http::StatusCode;
+    use warp::{http::StatusCode, http::Uri};
+
+    pub async fn get_url(slug: String, db: Db) -> Result<impl warp::Reply, Infallible> {
+        let urls = db.lock().await;
+        for url in urls.iter() {
+            if url.slug == slug {
+                let location = url.url.parse::<Uri>().unwrap();
+                return Ok(warp::redirect(location));
+            }
+        }
+        Ok(warp::redirect(Uri::from_static("/not_found")))
+    }
 
     pub async fn create_url(url: Url, db: Db) -> Result<impl warp::Reply, Infallible> {
         let mut vec = db.lock().await;
         vec.push(url);
-        return Ok(StatusCode::CREATED)
+        Ok(StatusCode::CREATED)
     }
 }
 
@@ -63,8 +92,7 @@ mod models {
 
     #[derive(Debug, Deserialize, Serialize, Clone)]
     pub struct Url {
-        url: String,
-        slug: String
+        pub url: String,
+        pub slug: String,
     }
-    
 }
